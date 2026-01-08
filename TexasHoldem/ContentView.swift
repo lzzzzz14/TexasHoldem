d978a04a5225dfba87d19b4f552a5ce42b7d930c
lizzz <1257041806@qq.com>
2026-01-08 23:32:16 +0800

    Initial Commit

---
+++ TexasHoldem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TexasHoldem
+//
+//  Created by 李直 on 2026/1/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
